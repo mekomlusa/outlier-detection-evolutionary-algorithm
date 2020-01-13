da9871af8e7bdb9d4a10495455664ebacf776b83
mekomlusaa <contact@roselin.me>
2020-01-13 08:48:25 -0600

    Added support to dataframe in the brute force algorithm

--- evood/models/brute_force.py
+++ evood/models/brute_force.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import itertools
 from collections import defaultdict
+import copy
 
 from evood.utils import sparsity_coeff, enumerate_subsets
 
@@ -34,8 +35,12 @@ class BruteForce:
 
         """
 
-        # accommodate Pandas DataFrame. What to do about feature names???
+        # accommodate Pandas DataFrame.
+        feature_names = []
+        is_dataframe = False
         if isinstance(data, pd.DataFrame):
+            is_dataframe = True
+            feature_names = data.columns.values.tolist()
             data = data.values
 
         assert (k <= data.shape[1]), "ValueError: number of sub-dimensions to look at is {}, exceed the original data dimension {}".format(k, data.shape[1])
@@ -48,11 +53,11 @@ class BruteForce:
         for i in range(len(perm_cols)):
             grid_ranges = list(itertools.product([x for x in range(phi)], repeat=len(perm_cols[i])))
             for g in grid_ranges:
-                sparsity_coeff_score, grid_ranges, num_of_records = sparsity_coeff(data, phi, perm_cols[i], g)
+                sparsity_coeff_score, trans_grid_ranges, num_of_records = sparsity_coeff(data, phi, perm_cols[i], g)
                 sparsity_coeff_dict[tuple(perm_cols[i])][g] = {}
                 sparsity_coeff_dict[tuple(perm_cols[i])][g]['sparsity_coeff_score'] = sparsity_coeff_score
                 sparsity_coeff_dict[tuple(perm_cols[i])][g]['num_records'] = num_of_records
-                orig_grid_ranges_dict.update(grid_ranges)
+                orig_grid_ranges_dict.update(trans_grid_ranges)
 
         # sort by values
         score_df = pd.DataFrame.from_dict({(i, j): sparsity_coeff_dict[i][j]
@@ -60,6 +65,13 @@ class BruteForce:
                                            for j in sparsity_coeff_dict[i].keys()},
                                           orient='index').reset_index()
         score_df.columns = ['columns_selected', 'grid_ranges', 'sparsity_coeff_score', 'num_records']
+
+        # updated display values for dataframe objects
+        if is_dataframe:
+            score_df['columns_selected'] = score_df['columns_selected'].apply(lambda x: self.feature_convert_helper(x, feature_names))
+            fixed_grid_ranges_dict = {feature_names[k]: v for k, v in orig_grid_ranges_dict.items()}
+            orig_grid_ranges_dict = copy.deepcopy(fixed_grid_ranges_dict)
+
         score_df = score_df.sort_values(by=['sparsity_coeff_score'], axis=0)
         # only keep values that have more than 0 data points.
         df_return = score_df[score_df['num_records'] > 0].iloc[:m]
@@ -69,3 +81,6 @@ class BruteForce:
         self.grid_ranges_dict = orig_grid_ranges_dict
 
         return self.score_df, self.grid_ranges_dict
+
+    def feature_convert_helper(self, col_nums, orig_feature_names):
+        return tuple([orig_feature_names[x] for x in col_nums])
